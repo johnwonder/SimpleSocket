fa1c9a487073a16a5293b131666f8da981b2ba0f
johnwonder <johnufida@163.com>
2015-08-25 23:16:40 +0800

    server easy method

---
+++ SimpleSocket/EasySocketServer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SimpleSocket
+{
+    class EasySocketServer
+    {
+        public void Start()
+        {
+            int port = 6000;
+            string host = "127.0.0.1";
+            IPAddress ip = IPAddress.Parse(host);
+            IPEndPoint ipe = new IPEndPoint(ip, port);
+
+            Socket sSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sSocket.Bind(ipe);
+            sSocket.Listen(0);
+            Console.WriteLine("监听已经打开，请等待！");
+
+            Socket serverSocket = sSocket.Accept();
+            Console.WriteLine("链接已经建立");
+            string recStr = "";
+            byte[] recByte = new byte[4096];
+            int bytes = serverSocket.Receive(recByte, recByte.Length, 0);
+            recStr += Encoding.ASCII.GetString(recByte, 0, bytes);
+
+            Console.WriteLine("服务器获得信息：{0}",recStr);
+            string sendStr = "send to client:hello";
+            byte[] sendByte = Encoding.ASCII.GetBytes(sendStr);
+            serverSocket.Send(sendByte, sendByte.Length, 0);
+            serverSocket.Close();
+            sSocket.Close();
+        }
+    }
+}
