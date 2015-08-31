659a39a01b3a1fbad22e0e655b85a173f3ff7ce7
johnwonder <johnufida@163.com>
2015-08-31 17:30:37 +0800

    simple session stack

---
+++ NetworkSocket/SimpleServer/FreeSessionStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleServer
+{
+    internal class FreeSessionStack<T>:IDisposable where T : SimpleSessionBase
+    {
+        private ConcurrentStack<T> stack = new ConcurrentStack<T>();
+
+        public int Count
+        {
+           get
+            {
+                return this.stack.Count;
+            }
+        }
+
+        public void Add(T session)
+        {
+            this.stack.Push(session);
+        }
+
+        public T Take()
+        {
+            T session;
+            if(this.stack.TryPop(out session))
+            {
+                return session;
+            }
+            return null;
+        }
+
+        public void Dispose()
+        {
+            var sessions = this.stack.ToArray();
+            foreach (var session in sessions)
+            {
+                IDisposable disposable = session;
+                disposable.Dispose();
+            }
+        }
+    }
+}
--- NetworkSocket/SimpleServer/SimpleServer.cs
+++ NetworkSocket/SimpleServer/SimpleServer.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace SimpleServer
 {
-    public class SimpleServer
+    public class SimpleServer<T>
     {
         private Socket listenSocket;
 
@@ -55,8 +55,56 @@ namespace SimpleServer
                 //InterLocked
             }
 
+            //处理后继续接收
             this.AcceptSession(arg);
         }
 
+        /// <summary>
+        /// 空闲客户端池
+        /// </summary>
+        private FreeSessionStack<T> freeSessionStack = new FreeSessionStack<T>();
+        /// <summary>
+        /// 从空闲池中取出或创建Session会话对象
+        /// </summary>
+        /// <returns></returns>
+        private T TakeOrCreateSession()
+        {
+            var session = this.freeSessionStack.Take();
+            if (session != null)
+                return session;
+
+            try
+            {
+                session = this.OnCreateSession();
+                if(session != null)
+                {
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return session;
+        }
+
+        protected abstract T OnCreateSession();
+
+        private void InitSession(T session,Socket socket)
+        {
+            //session.ReciveHandler =>(buffer) => this.OnReceive(session,buffer);
+            //session.DisconnectHandler =>() => this.RecyceSession(session);
+            //session.CloseHandler = () => this.RecyceSession(session);
+
+            // SocketAsync 与socket绑定
+            session.Bind(socket);
+            //添加到活动列表
+            (this.AllSessions as ICollection<T>).Add(session);
+            //通知已连接
+            this.OnConnect(session);
+            //开始接收数据
+            session.TryReceive();
+        }
     }
 }
